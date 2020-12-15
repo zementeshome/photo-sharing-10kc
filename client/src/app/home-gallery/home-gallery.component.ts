b84d917d3457b9bb499a1b1e3a4728f1aec72459
Zemen Teshome <zementeshome@gmail.com>
2020-12-15 16:57:37 -0500

    gallery card styled and responsive

--- client/src/app/home-gallery/home-gallery.component.ts
+++ client/src/app/home-gallery/home-gallery.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faMeh } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home-gallery',
   templateUrl: './home-gallery.component.html',
   styleUrls: ['./home-gallery.component.scss']
 })
 export class HomeGalleryComponent implements OnInit {
+  faMeh = faMeh
 
   constructor() { }
 
